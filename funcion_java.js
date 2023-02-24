88cbeecd536b07be1898dd94924f50974742d216
claudio <c-m_a@live.com>
2023-02-24 17:22:26 -0300

    primera entrega

---
+++ funcion_java.js
@@ -0,0 +1,5 @@
+document.getElementById('botoninicio').onclick=function ()
+{
+    console.log("capturamos botoninicio");    
+    location.href="pagina_informacion.html"
+}
---
+++ funcion_java_2.js
@@ -0,0 +1,26 @@
+document.getElementById('botoninfo').onclick=function ()
+{
+        console.log("capturamos botoninfo");
+        document.getElementById("infodemo").innerHTML= "<p>Información Personal:</p> <p>Id commodi odit et quia maximenon facere aut cumque unde et praesentium nostrum. 33 omnis maiores Est possimus non voluptas nisi et fugit unde sed quae explicabo ut molestias ipsa.</p> <p>Qui corporis tenetur Ut magnam aut accusantium facere ea totam voluptatibus eos enim praesentium id molestias quas et quis molestiae. Et laborum praesentium Et cumque sit architecto placeat sed laborum voluptatem. Aut dolore consequatur Non necessitatibus ut laboriosam amet sit porro consequatur qui sint galisum sit nesciunt facilis. Eum fugiat quiaEt perferendis eos autem accusantium aut sunt iste ut nihil reiciendis quo consectetur illum et culpa facilis. Sit voluptatem sintEa fugiat et animi fugit qui quisquam eius et voluptatibus excepturi. </p><p>Lorem ipsum dolor sit amet. Aut repellat maxime sed suscipit possimuseos modi id dolorem iusto. Qui dolorum minus Ut necessitatibus et tempora maiores sit excepturi voluptas! Et optio laborumEum harum ut reprehenderit consectetur nam delectus molestiae ea adipisci quos non aliquid tempore. Non quam numquamEt possimus et dolores expedita sit modi reprehenderit in velit perferendis et ipsa reprehenderit.</p> <p>Et omnis itaque non maiores vero Eos eaque ut saepe dolorem et esse labore et enim soluta ea laborum reiciendis. Qui natus placeat est galisum molestiaeUt numquam ab debitis dolor non nihil maxime non illo tempore rem voluptatum quisquam. Qui eius consectetur Ad similique aut dolores mollitia. Ut reiciendis facere eum dolores consequaturEos neque et sunt asperiores eos voluptas ipsum et asperiores molestiae?</p> <p> Sit voluptatibus pariatur Et quia cum nostrum atque est totam eveniet qui voluptatem mollitia ut aliquam quam! Aut beatae excepturiQui voluptatem aut corrupti consequatur ut veniam molestiae aut veritatis excepturi. Et neque rerum Sed quae aut dolor asperiores et deleniti omnis et consectetur cupiditate et consequatur corporis quo eaque doloribus! Vel fuga cumqueSit impedit et dolores atque aut tenetur deleniti et impedit laboriosam est velit perspiciatis et debitis vitae.</p>";
+}
+
+document.getElementById('botoncerrar').onclick=function ()
+{
+        console.log("capturamos botoncerrar");
+        document.getElementById("infodemo").innerHTML= "";
+}
+document.getElementById('iconofacebook').onclick=function ()
+{
+        console.log("capturamos iconofacebook");
+        location.href="https://www.facebook.com/"
+}
+document.getElementById('iconoinstagram').onclick=function ()
+{
+        console.log("capturamos iconoinstagram");
+        location.href="https://www.instagram.com/"
+}
+document.getElementById('iconotwitter').onclick=function ()
+{
+        console.log("capturamos iconotwitter");
+        location.href="https://www.twitter.com/"
+}
